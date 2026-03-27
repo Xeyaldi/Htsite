78996d3bddceffcbb844276e4ffb6b5b8fb1d249
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-03-27 08:46:46 +0400

    Update 935f2afb.674bf194.js

--- assets/js/935f2afb.674bf194.js
+++ assets/js/935f2afb.674bf194.js
@@ -1,1 +1,33 @@
-"use strict";(self.webpackChunkharmonoid_website=self.webpackChunkharmonoid_website||[]).push([[53],{1109:e=>{e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","banner":null,"badge":false,"className":"docs-version-current","isLast":true,"docsSidebars":{"tutorialSidebar":[{"type":"link","label":"Tutorial Intro","href":"/docs/intro","docId":"intro"}]},"docs":{"intro":{"id":"intro","title":"Tutorial Intro","description":"Let\'s discover Docusaurus in less than 5 minutes.","sidebar":"tutorialSidebar"}}}')}}]);
+"use strict";
+
+(self.webpackChunkharmonoid_website = self.webpackChunkharmonoid_website || []).push([[53], {
+    1109: e => {
+        e.exports = {
+            pluginId: "default",
+            version: "current",
+            label: "XeyalMusic v1.0",
+            banner: null,
+            badge: false,
+            className: "docs-version-current",
+            isLast: true,
+            docsSidebars: {
+                tutorialSidebar: [
+                    {
+                        type: "link",
+                        label: "XeyalMusic-ə Giriş",
+                        href: "/docs/intro",
+                        docId: "intro"
+                    }
+                ]
+            },
+            docs: {
+                intro: {
+                    id: "intro",
+                    title: "XeyalMusic-ə Giriş",
+                    description: "XeyalMusic sistemini 5 dakikadan az müddətdə kəşf edin.",
+                    sidebar: "tutorialSidebar"
+                }
+            }
+        };
+    }
+}]);
